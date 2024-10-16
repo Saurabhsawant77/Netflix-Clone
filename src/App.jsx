e38dc3954031d0168fb7fc9f92db5b77e8e16a1d
Saurabh Sawant <113126470+Saurabhsawant77@users.noreply.github.com>
2024-10-16 23:17:43 +0530

    Frontend Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="uppercase">
+      <h1 className="font-nsans-light">Netflix</h1>
+      <h1 className="font-nsans-bold">Netflix</h1>
+      <h1 className="font-nsans-medium">Netflix</h1>
+    </div>
+  )
+}
+
+export default App
